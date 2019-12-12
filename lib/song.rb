540a1eb1b87ee73ef49e6a6b6fa6ea349fd50861
Matthew Jones <matts3371@gmail.com>
2019-12-12 22:36:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,17 @@
 class Song
 
   attr_accessor :name, :artist
+  
+  @@all = [] 
 
   def initialize(name)
     @name = name
+    @@all << self 
   end
+  
+  def self.all 
+    @@all 
+  end 
 
   def self.new_by_filename(file)
     song_name = file.split(" - ")[1]
