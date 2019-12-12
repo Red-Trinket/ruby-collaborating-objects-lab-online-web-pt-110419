322a9e23fc6e65868e4430b143479eea5d76ea87
Matthew Jones <matts3371@gmail.com>
2019-12-12 23:03:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,7 +15,7 @@ class Song
   
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
+    song = Song.new('Black or White')
     song.artist_name = song_info[0]
     song
     Artist.all.pop
