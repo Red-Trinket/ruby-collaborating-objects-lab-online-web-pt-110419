b13bcbfbf0e6fb21ceebd447eed40fb44b6190ac
Matthew Jones <matts3371@gmail.com>
2019-12-12 23:00:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,12 +14,12 @@ class Song
   end 
   
   def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
     song
   end
+end
 
   def self.new_by_filenam(file)
     song_name = file.split(" - ")[1]
