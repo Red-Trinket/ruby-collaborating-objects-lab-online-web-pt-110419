3b0c54309f7634c9748a43560a1d56049fc33556
Matthew Jones <matts3371@gmail.com>
2019-12-12 22:31:20 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -57,7 +57,7 @@ class Artist
 
   #this method prints out an array of song names
   #by accessing the name attribute of each song
-  def print_songs()
+  def songs
     puts @songs.collect {|x| x.name}
   end
 
